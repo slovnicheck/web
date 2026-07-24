5f60baf70972d507de65072f14907a68f4cbcfa5
dezinformacnislovnicheck <admin@dezinformacnislovnicheck.cz>
2026-07-24 17:54:48 +0200

    .

--- games-data.js
+++ games-data.js
@@ -5,19 +5,58 @@ const gamesData = [
         image: "https://d35666577a.cbaul-cdnwnd.com/73803269aab4c81b82c674cc7d598ffb/200000074-c441dc441e/propaganda%20hra.png?ph=d35666577a",
         perex: "Kreativní hra, kde žáci v týmech vytvářejí vlastní propagandistické materiály na vylosované téma (černá, bílá nebo šedá propaganda).",
         content: `
-            <h3>Příprava (Pedagog)</h3>
-            <p>Před začátkem hry je důležité si látku o propagandě projít. Učitel napíše na papírky druhy propagandy: <strong>2× černá, 2× šedá, 2× bílá</strong> a nastříhá je.</p>
-            <h3>Průběh hry</h3>
-            <ol>
-                <li><strong>Rozdělení:</strong> Žáci se rozdělí do skupinek po cca 5 lidech (ideálně 6 skupin).</li>
-                <li><strong>Losování:</strong> Vyučující schová papírky nebo je dá do klobouku. Každá skupina si jeden vylosuje.</li>
-                <li><strong>Vymýšlení (5 min):</strong> Skupina si rozmyslí téma a zvolí "grafiky" pro kresbu plakátu.</li>
-                <li><strong>Realizace (5 min):</strong> Tvorba plakátu a sloganu podle vylosovaného typu propagandy.</li>
-                <li><strong>Prezentace:</strong> Každá skupina představí výtvor. Ostatní hádají, o jaký typ propagandy šlo.</li>
-            </ol>
-            <h3>Cíl hry</h3>
-            <p>Přenesení teoretických informací do praxe, zlepšení komunikace v týmu a rozvoj fantazie.</p>
-            <p><strong>Časová dotace:</strong> cca 30 minut (1 vyučovací hodina).</p>
+            <div class="Material" id="Propaganda">
+          <div class="MaterialNadpis">
+            <h2>Propaganda</h2>
+          </div>
+          <div class="MaterialContent">
+            <h5>Před začátkem hry je důležité si danou látku projít. Hra pracuje s&nbsp;nabitými znalostmi z&nbsp;tohoto tématu</h5>
+              <br>
+              <p> <b>Pedagog:</b>
+                <br>Před začátkem hry na papír napíše druhy propagandy: 2&nbsp;×&nbsp;černá, 2&nbsp;×&nbsp;šedá a 2&nbsp;×&nbsp;bílá,
+                které následně nastříhá.
+              </p>
+                <br>
+                <p><b>Průběh:</b>
+                <br> Rozdělte se do skupinek po pěti. Možný i&nbsp;menší počet lidí v&nbsp;jedné skupině. Cíl je
+                mít v&nbsp;jedné skupině alespoň 2&nbsp;-&nbsp;5 hráčů. Počet skupinek by tedy měl být 6. Vyučující dobře
+                skryje předem přichystané papírky s&nbsp;druhy propagandy a&nbsp;každé skupince nabídne jeden papír
+                (papírky lze vhodit i&nbsp;do klobouku/&nbsp;čepice, ze které si žáci mohou vybírat). Jakmile dostane
+                každá skupinka svůj papír s&nbsp;propagandou, je na řadě, aby si během 5&nbsp;minut rozmysleli téma,
+                o&nbsp;čem chtějí danou propagandu dělat. Ve skupince si také zvolí osoby, které se budou věnovat
+                kresbě plakátu (opět se toho můžou zúčastnit všichni, lepší pro orientaci jsou 2&nbsp;). Jakmile
+                uplyne 5 minut, nechá se dalších 5 minut na realizaci plakátů a&nbsp;textu k&nbsp;jednotlivým
+                propagandám ve&nbsp;skupinkách. Jakmile budou hotovi, je na řadě přednes před třídou. Každá
+                skupinka přednese svůj výtvor a&nbsp;ostatní skupinky tak budou hádat, jaký druh propagandy to
+                je. Na konci hodiny vybere vyučující samostatně jednu skupinku, která toto téma splnila
+                nejlépe. Vybrat nejlepší skupinku můžou i&nbsp;žáci. Mohou například vybrat tu skupinku, která je
+                svou propagandou nejvíce přesvědčila.
+              </p>
+                <br>
+              <p><b>Materiál vyučujícího:</b>
+                <br>
+                Výukový materiál Propaganda +&nbsp;infografika
+              </p>
+                <br>
+              <p><b>Cíl hry:</b> <br>
+                Přenesení získaných informací do praxe, zlepšení komunikace mezi žáky, rozvinutí fantazie
+              </p>
+                <br>
+              <p><b>Časová doba hraní:</b>
+                <br>30 minut (lze stihnout i&nbsp;dříve)
+               </p>
+                <br>
+              <p><b>Doporučená doba trvání:</b>
+              <br>1 vyučovací hodina</p>
+              <br>
+              <div class="MatImg">
+                <img draggable="false" src="./Hry_files/materialkehrepropaganda.png">
+              </div>
+          </div>
+        </div>
+
+
+      </div>
         `
     },
     {
@@ -26,23 +65,69 @@ const gamesData = [
         image: "https://images.unsplash.com/photo-1504711434969-e33886168f5c?auto=format&fit=crop&w=800&q=80", // Ilustrační - psaní/noviny
         perex: "Nejlepší obranou je útok. Žáci si zkusí sami vytvořit fake news, aby pochopili mechanismy, které manipulátoři používají.",
         content: `
-            <h3>Průběh hry</h3>
-            <p>Pracuje se ve skupinách nebo jednotlivě. Žáci si vyberou téma (kultura, zdravotnictví, politika...) a napíší krátký článek, který <strong>musí obsahovat dezinformační prvky</strong> (zastrašování, chybějící zdroje, emoce, gramatické chyby).</p>
-            <p>Na závěr se články čtou a vybírá se ten "nejuvěřitelnější".</p>
-            
-            <h3>Příklad (Téma: Zdravotnictví)</h3>
-            <blockquote>
-                "Budeme se muset znova očkovat?!! Vláda v ČR tajně odhlasovala znovuzavedení roušek kvůli viru z Afriky. Mluvčí (bez jména) říká, že je to proti právu! Porušení bude znamenat vězení! Sdílejte, než to smažou! -- Fantom xx"
-            </blockquote>
-            
-            <h3>Co v textu hledat (Chyby)</h3>
-            <ul>
-                <li>Chybí datum a autor.</li>
-                <li>Hodně subjektivní, vykřičníky.</li>
-                <li>Vyvolává strach a vztek.</li>
-                <li>Odvolává se na neexistující "mluvčí".</li>
-            </ul>
-            <p><strong>Cíl hry:</strong> Posílení kreativního myšlení a schopnost rozeznat manipulativní text.</p>
+          <div class="Material" id="Dezinfoclanek">
+          <div class="MaterialNadpis">
+            <h2>Tvorba vlastního dezinformačního článku</h2>
+          </div>
+          <div class="MaterialContent">
+            <h5>Před hrou je nezbytná znalost dezinformačních webů, čerpejte z&nbsp;našeho článku či&nbsp;výukových materiálů</h5>
+              <br>
+      <p><b>Průběh hry:</b>
+            <br>
+            Ve třídě, skupině lidí nebo jen tak mezi sebou (min. 2&nbsp;osoby) si vezměte
+            něco na psaní (doporučujeme čistý papír a tužku). Poté si každá skupinka nebo
+            jednotlivec (pokud hrajete ve dvou) vybere téma, o&nbsp;kterém bude chtít psát. Příklady
+            výběru z&nbsp;témat: kultura, zahraničí, ČR, státní zřízení, zdravotnictví apod. Téma výtvorů
+            můžete zvolit jakékoliv, z&nbsp;výše uvedených či&nbsp;vlastní. Následně se&nbsp;můžete pustit do
+            práce: každý jednotlivě nebo ve skupinkách (záleží, jak se dohodnete) budete pracovat
+            na vlastním dezinformačním článku.
+            <br>
+            Vytvoříte tedy článek, který má přesné dezinformační prvky (např. má zastrašit, uvést
+            do rozpaků, chybí zdroje apod.) Až budou články hotové, každý ve třídě či&nbsp;skupině
+            přečte svůj výtvor. Následně si můžete mezi sebou zvolit ten, který se vám líbí nejvíce.
+       </p>
+          <br>
+       <p><b>Příklad dezinformačního článku na téma zdravotnictví:</b>
+          <br>
+        <em>Budeme se muset znova očkovat?!! A&nbsp;proč to bude příští rok povinné?
+          Vláda v&nbsp;ČR odhlasovala v&nbsp;úzkém kroužku specifických lidí námět na znovuzavedení
+          roušek v&nbsp;ČR. Týkat se to bude nejen českých obyvatel, ale i návštěvníků této země.
+          Parlament v&nbsp;ČR 8.&nbsp;5&nbsp;. 2024 podal návrh, který by pro nás nebyl příhodný. Jedná se totiž o&nbsp;zavedení roušek, jakožto další obrana proti případnému onemocnění, které se k nám
+          šíří od Afriky. Mluvčí na toto vyjádření reagoval, že se za žádnou cenu nebude podřizovat
+          vládě a&nbsp;že usnesení, které provedla je v&nbsp;rozporu s&nbsp;občanským právem. Dále také vyzívá
+          občany, aby se postavili proti vládě a&nbsp;vyjádřili stejně jako on nesouhlas.
+          <br>
+          
+          Nová opatření proti blížícímu se viru, který je údajně podobný jako nám známý Covid-
+          19, by se měla zavést už od příštího roku. Mezi ně budou patřit i&nbsp;zmiňované roušky.
+          <br>
+          Měly by se tak nosit v&nbsp;uzavřených místnostech, jakožto školách, nemocnicích a&nbsp;
+          podobných zařízeních, dokonce by se toto opatření mělo přenést i&nbsp;na venek. V&nbsp;tomto
+          případě by to znamenalo, že bez roušky už nesmíme nikam!
+          Porušení těchto opatření by pak mělo za následek tučnou pokutu či dokonce vězení,
+          uvedl Radek.
+          Fantom xx</em>
+        <br>
+     </p>
+      <br>
+      <p><b>Chyby: </b>
+        <br> chybí datum, hodně subjektivní, vyvolává pocit strachu a rozčilení, chybí jméno
+        autora textu atd.</p>
+      <br>
+    <p><b>Časová doba přípravy:</b>
+      <br>
+      Doporučená doba na zpracování je 30 minut, pokud ale chcete,
+      můžete na tom pracovat po čas, který si vy sami zvolíte.
+     </p>
+      <br>
+    <p><b>Cíl hry:</b>
+      <br>Cílem krom posílení kreativního myšlení a&nbsp;psaní, je i&nbsp;schopnost rozeznat, co je a&nbsp;co není dezinformační zpráva.
+    </p>
+
+          </div>
+        </div>
+      </div>
+ 
         `
     }
 ];
